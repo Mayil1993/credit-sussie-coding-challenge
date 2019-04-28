e866ce1c403ab06b0f80e4d810919201b30b4f0b
Mayil Vahanan <mavah93@gmail.com>
2019-04-28 22:59:46 +0800

    mvn plugin

--- src/main/java/GraphDrawingService/com/graphService/com/Controller.java
+++ src/main/java/GraphDrawingService/com/graphService/com/Controller.java
@@ -15,18 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/draw")
 public class Controller {
 	
-	
 	@RequestMapping(value = "/createCanvas", method = RequestMethod.POST)
 	public Object createCanvas(@RequestBody CanvasVO canvasVo) {
 		Canvas canvas = new Canvas();
 		TransformDTO dto = new TransformDTO();
-		System.out.println("==========>"+canvasVo.getCommand());
 		String command = canvasVo.getCommand();
         String[] inpArray;
             
                 	inpArray = command.split(" ");
                     char[][] lineArray = canvas.drawCanvas(Integer.parseInt(inpArray[1]),Integer.parseInt(inpArray[2]));
-                    System.out.println(canvas.render(lineArray));
                     dto.setCanvasArray(lineArray);
 		return dto;
 	}
@@ -35,13 +32,10 @@ public class Controller {
 		public Object drawLine(@RequestBody LineVO lineVo) {
 		Canvas canvas = new Canvas();
 		TransformDTO dto = new TransformDTO();
-		System.out.println("==========>"+lineVo.getCommand());
-		System.out.println("==========>"+lineVo.getCanvasArray());
 		
 		String command = lineVo.getCommand();
 		 String[] inpArray = command.split(" ");
 		char[][] lineArray = canvas.drawLine(lineVo.getCanvasArray(),Integer.parseInt(inpArray[1]),Integer.parseInt(inpArray[2]),Integer.parseInt(inpArray[3]),Integer.parseInt(inpArray[4]),'X');
-        System.out.println(canvas.render(lineArray));
         	dto.setCanvasArray(lineArray);
 		return dto;
 	}
@@ -50,13 +44,10 @@ public class Controller {
 	public Object drawRectangle(@RequestBody LineVO lineVo) {
 	Canvas canvas = new Canvas();
 	TransformDTO dto = new TransformDTO();
-	System.out.println("==========>"+lineVo.getCommand());
-	System.out.println("==========>"+lineVo.getCanvasArray());
 	
 	String command = lineVo.getCommand();
 	 String[] inpArray = command.split(" ");
 	char[][] lineArray = canvas.drawRectangle(lineVo.getCanvasArray(),Integer.parseInt(inpArray[1]),Integer.parseInt(inpArray[2]),Integer.parseInt(inpArray[3]),Integer.parseInt(inpArray[4]),'X');
-    System.out.println(canvas.render(lineArray));
     dto.setCanvasArray(lineArray);
 	return dto;
 }
@@ -65,13 +56,10 @@ public class Controller {
 	public Object bucketFill(@RequestBody LineVO lineVo) {
 	Canvas canvas = new Canvas();
 	TransformDTO dto = new TransformDTO();
-	System.out.println("==========>"+lineVo.getCommand());
-	System.out.println("==========>"+lineVo.getCanvasArray());
 	
 	String command = lineVo.getCommand();
 	 String[] inpArray = command.split(" ");
 	char[][] lineArray =  canvas.bucketFill(lineVo.getCanvasArray(),Integer.parseInt(inpArray[1]),Integer.parseInt(inpArray[2]),inpArray[3].charAt(0));
-    System.out.println(canvas.render(lineArray));
     dto.setCanvasArray(lineArray);
 	return dto;
 }
@@ -79,12 +67,8 @@ public class Controller {
 	@RequestMapping(value = "/printGraph", method = RequestMethod.POST)
 	public String printGraph(@RequestBody LineVO lineVo) {
 	Canvas canvas = new Canvas();
-	System.out.println("==========>"+lineVo.getCommand());
-	System.out.println("==========>"+lineVo.getCanvasArray());
 	String outPut = "";
 	outPut =  canvas.render(lineVo.getCanvasArray());
-    System.out.println(outPut);
-
 	return outPut;
 }
 	 
--- src/main/java/model/Canvas.java
+++ src/main/java/model/Canvas.java
@@ -1,23 +1,6 @@
 package model;
 
 public class Canvas {
-    char[][] canvasArray;
-    int w, h;
-    public Canvas(){}
-    public Canvas(int w, int h) {
-        if(w < 1 || h < 1) {
-            throw new DrawingGraphIOException("Canvas width and height can't be 0");
-        }
-        h+=2;
-        w+=2;
-        this.w = w;
-        this.h = h;
-        this.canvasArray = new char[h][w];
-        drawLine(0, 0, this.w-1, 0, '-');
-        drawLine(0, this.h-1, this.w-1, this.h-1, '-');
-        drawLine(0, 1, 0, this.h-2, '|');
-        drawLine(this.w-1, 1, this.w-1, this.h-2, '|');
-    }
     
     public char[][] drawCanvas(int width, int height){
     	height+=2;
@@ -31,26 +14,12 @@ public class Canvas {
     	
     	return lineArray;
     }
-    public String render() {
-        checkCanvas();
-        StringBuilder strBuilder = new StringBuilder();
-        for(int i=0;i<this.h;i++) {
-            for(int j=0;j<this.w;j++) {
-                strBuilder.append(this.canvasArray[i][j] == '\u0000'?' ':this.canvasArray[i][j]);
-            }
-            strBuilder.append("\n");
-        }
-        return strBuilder.toString().trim();
-    }
-    
     
     public String render(char[][] lineArray) {
-        checkCanvas(lineArray);
+        validateCanvas(lineArray);
         StringBuilder strBuilder = new StringBuilder();
         int height = lineArray.length;
-        System.out.println("height=====>"+height);
         int width = lineArray[0].length;
-        System.out.println("width=====>"+width);
         
         for(int i=0;i<height;i++) {
             for(int j=0;j<width;j++) {
@@ -62,17 +31,9 @@ public class Canvas {
     }
     
     
-    public void drawLine(int x1, int y1, int x2, int y2, char mChar) {
-        checkCanvas();
-        for(int i=y1; i<=y2; i++) {
-            for(int j=x1; j<=x2; j++) {
-                this.canvasArray[i][j] = mChar;
-            }
-        }
-    }
     
     public char[][] drawLine(char[][] lineArray,int x1, int y1, int x2, int y2, char mChar) {
-        checkCanvas(lineArray);
+        validateCanvas(lineArray);
         for(int i=y1; i<=y2; i++) {
             for(int j=x1; j<=x2; j++) {
             	lineArray[i][j] = mChar;
@@ -80,17 +41,10 @@ public class Canvas {
         }
         return lineArray;
     }
-    
-    public void drawRectangle(int x1, int y1, int x2, int y2, char mchar) {
-        checkCanvas();
-        drawLine(x1,y1, x2, y1, mchar);
-        drawLine(x1,y1, x1, y2, mchar);
-        drawLine(x2,y1, x2, y2, mchar);
-        drawLine(x1,y2, x2, y2, mchar);
-    }
+  
     
     public char[][] drawRectangle(char[][] lineArray, int x1, int y1, int x2, int y2, char mchar) {
-        checkCanvas(lineArray);
+        validateCanvas(lineArray);
         lineArray = drawLine(lineArray,x1,y1, x2, y1, mchar);
         lineArray = drawLine(lineArray,x1,y1, x1, y2, mchar);
         lineArray = drawLine(lineArray,x2,y1, x2, y2, mchar);
@@ -98,23 +52,9 @@ public class Canvas {
         
         return lineArray;
     }
-    public void bucketFill(int x, int y, char mchar) {
-        checkCanvas();
-        if((int)this.canvasArray[y][x] != 0) {
-            return;
-        }
-        if(x > 0 || x < this.h || y > 0 || y  < this.w) {
-            if((int)this.canvasArray[y][x] == 0)
-                this.canvasArray[y][x] = mchar;
-            bucketFill(x+1,y, mchar);
-            bucketFill(x-1,y, mchar);
-            bucketFill(x,y-1, mchar);
-            bucketFill(x,y+1, mchar);
-        }
-    }
     
     public char[][] bucketFill(char[][] lineArray,int x, int y, char mchar) {
-        checkCanvas(lineArray);
+        validateCanvas(lineArray);
         int height = lineArray.length-1;
         int width = lineArray[0].length-1;
         if((int)lineArray[y][x] != 0) {
@@ -131,12 +71,7 @@ public class Canvas {
         return lineArray;
     }
 
-    private void checkCanvas() {
-        if(this.canvasArray == null)
-            throw new DrawingGraphIOException("Draw a canvas first");
-    }
-    
-    private void checkCanvas(char[][] lineArray) {
+    private void validateCanvas(char[][] lineArray) {
         if(lineArray == null)
             throw new DrawingGraphIOException("Draw a canvas first");
     }
